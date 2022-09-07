cd4f91103dea737868c8709a3928bb53e06a47d4
nickvhoy11 <nicholas.hoyecki@uconn.edu>
2022-09-07 13:14:39 -0400

    Green

--- src/main/java/net/nickvhoy11/deathsaver/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/nickvhoy11/deathsaver/mixin/ServerPlayerEntityMixin.java
@@ -1,28 +1,27 @@
 package net.nickvhoy11.deathsaver.mixin;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import static net.nickvhoy11.deathsaver.DeathSaver.LOGGER;
 import net.minecraft.text.Text;
 
 
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
 
-    @Inject(at = @At("HEAD"),method = "onDeath")
+    @Inject(at = @At("TAIL"),method = "onDeath")
     private void onDeath(DamageSource damageSource,CallbackInfo info) {
         String xpos = String.valueOf((int)(((ServerPlayerEntity)(Object)this).prevX));
         String ypos = String.valueOf((int)(((ServerPlayerEntity)(Object)this).prevY));
         String zpos = String.valueOf((int)(((ServerPlayerEntity)(Object)this).prevZ));
-        //String pname = ((ServerPlayerEntity)(Object)this).getPlayerListName().toString();
-        //pname+"died at "+
-        String finalStr = "["+xpos+","+ypos+","+zpos+"]";
+        //String player = ((ServerPlayerEntity)(Object)this)..toString();
+        String finalStr ="["+xpos+","+ypos+","+zpos+"]";
 
-        LOGGER.info(finalStr);
-        ((ServerPlayerEntity)(Object)this).getServer().getPlayerManager().broadcast(Text.literal(finalStr),false);
+        //LOGGER.info(finalStr);
+        ((ServerPlayerEntity)(Object)this).getServer().getPlayerManager().broadcast(Text.literal(finalStr).formatted(Formatting.GREEN),false);
     }
 }
 
